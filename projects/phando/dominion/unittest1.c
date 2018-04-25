bf6d33504fb4b07428e42c150e687bf1d6a3d983
Dominic <dvphan@ucdavis.edu>
2018-04-24 18:19:42 -0700

    finished unit test 1,2,3

--- projects/phando/dominion/unittest1.c
+++ projects/phando/dominion/unittest1.c
@@ -1,5 +1,5 @@
 // Dominic Phan
-// Testing fullDeckCount()
+// Testing supplyCount()
 
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -8,44 +8,64 @@
 #include <assert.h>
 #include "rngs.h"
 
-void asserttrue(int booleanStatement) {
-  if (booleanStatement) {
+int totalFail = 0;
+
+void asserttrue(int a, int b) {
+  if (a == b) {
     printf("Assert Passed\n");
   } else {
-    printf("Assert failed\n");
+    printf("Assert failed, total fails: %d\n", totalFail);
+    totalFail++;
   }
 }
 
-// set NOISY_TEST to 0 to remove printfs from output
-#define NOISY_TEST 1
-
 int main(){
-  int i;
   int seed = 1000;
-  int numPlayer = 2;
-  int p, r, handCount;
+  int numPlayer = 4;
+  int p, r;
+  // select kingdom cards
   int k[10] = {adventurer, council_room, feast, gardens, mine,
                remodel, smithy, village, baron, great_hall};
   struct gameState G;
-  int maxHandCount = 5;
-  int coppers[MAX_HAND];
-  int silvers[MAX_HAND];
-  int golds[MAX_HAND];
 
-  for (i = 0; i < MAX_HAND; i++) {
-    coppers[i] = copper;
-    silvers[i] = silver;
-    golds[i] = gold;
-  }
-  printf("TESTING fullDeckCount():\n");
-  for (p = 0; p < numPlayer; p++){
+  /* The supplyCount() function must return the correct amount of supply cards currently available.
+  * This is important because when 3 supplies are depleted, the game ends, so this function must work 
+  ** correctly.
+  * Things to note: victory cards and curses have different numbers of cards depending on numPlayer
+  *                 curse cards = (numPlayers-1) * 10
+  *                 victory cards = 8 for two players; 12 for 3+
+  *                 resource cards = 30 gold, 40, silver; 60 - (numplayer * 7)
+  *                 nonvictory kingdom cards = 10
+  */
+  printf("TESTING supplyCount():\n");
+  // testing for players 2, 3, 4
+  for (p = 2; p <= numPlayer; p++){
     memset(&G, 23, sizeof(struct gameState)); // clear game state
     r = initializeGame(numPlayer, k, seed, &G); // initialize new game
-    handCount = maxHandCount;
-    G.handCount[p] = handCount;
-    memcpy(G.hand[p], coppers, sizeof(int) * handCount); // setting all cards to copper
-    fullDeckCount(p, 4);
+    printf("Testing supplyCount() with %d players\n", numPlayer);
+    // Check resource cards
+    printf("Checking copper, silver, gold\n");
+    asserttrue(supplyCount(copper, &G), (60 - (7*numPlayer)));
+    asserttrue(supplyCount(silver, &G), 40);
+    asserttrue(supplyCount(gold, &G), 30);
+    printf("Checking curse cards\n");
+    asserttrue(supplyCount(curse, &G), (numPlayer-1) * 10);
+    printf("Checking a kingdom card\n");
+    asserttrue(supplyCount(feast, &G), 10);
+    printf("Checking a victory card\n");
+    if (numPlayer == 2) {
+
+      asserttrue(supplyCount(gardens, &G), 8);
+    }
+    else {
+      asserttrue(supplyCount(gardens, &G), 12);
+    }
 
   }
-
+  if (totalFail == 0) {
+    printf("FINAL RESULT: All tests passed\n");
+  } else {
+    printf("FINAL RESULT: %d FAILURES\n", totalFail);
+  }
+  return 0;
 }
