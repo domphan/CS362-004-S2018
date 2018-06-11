ad14e173f154b2114a6f5eecae09d68a587246b9
Dominic <dvphan@ucdavis.edu>
2018-06-10 19:08:04 -0700

    created unit tests

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -25,19 +25,143 @@ public class UrlValidatorTest extends TestCase {
 //You can use this function to implement your manual testing
         UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
         System.out.println("Testing Manually: ");
+        System.out.println(urlVal.isValid("http://www.google.com/hello/"));
     }
 
-    // First partition will be valid URLs
-    public void testYourFirstPartition() {
-        //You can use this function to implement your First Partition testing
+
+    public void testScheme() {
+        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        urlparts createdURL = new urlparts("", true); // default value true, will set false inside logic
+        System.out.println("Testing Schemes:");
+        for (urlparts scheme:scheme) {
+            if (!scheme.value) {
+                createdURL.value = false;
+            } else {
+                createdURL.value = true;
+            }
+            createdURL.part = scheme.part + authority[0].part + port[0].part + path[0].part + query[0].part;
+            try {
+                if (createdURL.value) {
+                    assertTrue(urlVal.isValid(createdURL.part));
+                } else {
+                    assertFalse(urlVal.isValid(createdURL.part));
+                }
+            } catch (Throwable t) {
+                System.out.println(createdURL.part + " assertion failed");
+                System.out.println("Expected: " + createdURL.value);
+                System.out.println("Result: " + !createdURL.value);
+                System.out.println("Input: scheme: " + scheme.value + " | authority: " + authority[0].value + " | port: " + port[0].value + " | path: " + path[0].value + " | query: " + query[0].value + "\n");
+            }
+        }
+
 
     }
-    // second partition will be invalid URLs
-    public void testYourSecondPartition() {
-        //You can use this function to implement your Second Partition testing
+
+    public void testAuthority() {
+        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        urlparts createdURL = new urlparts("", true); // default value true, will set false inside logic
+        System.out.println("Testing Authorities:");
+        for (urlparts authority:authority) {
+            if (!authority.value) {
+                createdURL.value = false;
+            } else {
+                createdURL.value = true;
+            }
+            createdURL.part = scheme[0].part + authority.part + port[0].part + path[0].part + query[0].part;
+            try {
+                if (createdURL.value) {
+                    assertTrue(urlVal.isValid(createdURL.part));
+                } else {
+                    assertFalse(urlVal.isValid(createdURL.part));
+                }
+            } catch (Throwable t) {
+                System.out.println(createdURL.part + " assertion failed");
+                System.out.println("Expected: " + createdURL.value);
+                System.out.println("Result: " + !createdURL.value);
+                System.out.println("Input: scheme: " + scheme[0].value + " | authority: " + authority.value + " | port: " + port[0].value + " | path: " + path[0].value + " | query: " + query[0].value + "\n");
+            }
+        }
 
     }
-    //You need to create more test cases for your Partitions if you need to
+
+    public void testPort(){
+        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        urlparts createdURL = new urlparts("", true); // default value true, will set false inside logic
+        System.out.println("Testing Ports:");
+        for (urlparts port:port) {
+            if (!port.value) {
+                createdURL.value = false;
+            } else {
+                createdURL.value = true;
+            }
+            createdURL.part = scheme[0].part + authority[0].part + port.part + path[0].part + query[0].part;
+            try {
+                if (createdURL.value) {
+                    assertTrue(urlVal.isValid(createdURL.part));
+                } else {
+                    assertFalse(urlVal.isValid(createdURL.part));
+                }
+            } catch (Throwable t) {
+                System.out.println(createdURL.part + " assertion failed");
+                System.out.println("Expected: " + createdURL.value);
+                System.out.println("Result: " + !createdURL.value);
+                System.out.println("Input: scheme: " + scheme[0].value + " | authority: " + authority[0].value + " | port: " + port.value + " | path: " + path[0].value + " | query: " + query[0].value + "\n");
+            }
+        }
+    }
+
+    public void testPath() {
+        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        urlparts createdURL = new urlparts("", true); // default value true, will set false inside logic
+        System.out.println("Testing Paths:");
+        for (urlparts path:path) {
+            if (!path.value) {
+                createdURL.value = false;
+            } else {
+                createdURL.value = true;
+            }
+            createdURL.part = scheme[0].part + authority[0].part + port[0].part + path.part + query[0].part;
+            try {
+                if (createdURL.value) {
+                    assertTrue(urlVal.isValid(createdURL.part));
+                } else {
+                    assertFalse(urlVal.isValid(createdURL.part));
+                }
+            } catch (Throwable t) {
+                System.out.println(createdURL.part + " assertion failed");
+                System.out.println("Expected: " + createdURL.value);
+                System.out.println("Result: " + !createdURL.value);
+                System.out.println("Input: scheme: " + scheme[0].value + " | authority: " + authority[0].value + " | port: " + port[0].value + " | path: " + path.value + " | query: " + query[0].value + "\n");
+            }
+        }
+    }
+
+    public void testQuery() {
+        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        urlparts createdURL = new urlparts("", true); // default value true, will set false inside logic
+        System.out.println("Testing Queries:");
+        for (urlparts query:query) {
+            if (!query.value) {
+                createdURL.value = false;
+            } else {
+                createdURL.value = true;
+            }
+            createdURL.part = scheme[0].part + authority[0].part + port[0].part + path[0].part + query.part;
+            try {
+                if (createdURL.value) {
+                    assertTrue(urlVal.isValid(createdURL.part));
+                } else {
+                    assertFalse(urlVal.isValid(createdURL.part));
+                }
+            } catch (Throwable t) {
+                System.out.println(createdURL.part + " assertion failed");
+                System.out.println("Expected: " + createdURL.value);
+                System.out.println("Result: " + !createdURL.value);
+                System.out.println("Input: scheme: " + scheme[0].value + " | authority: " + authority[0].value + " | port: " + port[0].value + " | path: " + path[0].value + " | query: " + query.value + "\n");
+            }
+        }
+    }
+
 
     public void testIsValid() {
         //You can use this function for programming based testing
@@ -88,12 +212,17 @@ public class UrlValidatorTest extends TestCase {
 
         UrlValidatorTest fct = new UrlValidatorTest("url test");
         fct.testManualTest();
-        fct.testIsValid();
+        //fct.testIsValid();
+        fct.testScheme();
+        fct.testAuthority();
+        fct.testPort();
+        fct.testPath();
+        fct.testQuery();
     }
 
     urlparts[] scheme = {
-            new urlparts("https://", true),
             new urlparts("http://", true),
+            new urlparts("https://", true),
             new urlparts("u6p:/", false)
     };
     urlparts[] authority = {
@@ -111,7 +240,7 @@ public class UrlValidatorTest extends TestCase {
             new urlparts("/]", false)
     };
     urlparts[] query = {
-            new urlparts("?Hello=new", true),
-            new urlparts("", true)
+            new urlparts("", true),
+            new urlparts("?Hello=new", true)
     };
 }
